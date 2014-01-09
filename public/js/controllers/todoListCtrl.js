11160456b71c4ee81c63fd6491cac2bc42127742
Zygonie <github@zygonie.com>
2014-01-09 15:49:27 -0500

    Refactoring and regex check for URL in a task

--- public/js/controllers/todoListCtrl.js
+++ public/js/controllers/todoListCtrl.js
@@ -10,7 +10,9 @@
     $scope.doneTasksPresent = false;
     $scope.toFocus = false;
 
-    //Spinner
+    /***************
+     *** Spinner ***
+     ***************/
     var opts = {
         lines: 13, // The number of lines to draw
         length: 20, // The length of each line
@@ -50,10 +52,68 @@
         return false;
     };
     
+    /*************
+     *** Tools ***
+     *************/
     function isDefined(x) {
         var undefined;
         return x !== undefined;
     }
+
+    function isUrl(s) {
+        var regexp = /((http|https|ftp)\:\/\/)?([a-zA-Z0-9\.\-]+(\:[a-zA-Z0-9\.&amp;%\$\-]+)*@)*((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|localhost|([a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(\:[0-9]+)*(\/($|[a-zA-Z0-9\.\,\?\'\\\+&amp;%\$#\=~_\-]+))*$/;
+        //var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+        return regexp.test(s);
+    }
+
+    function urlStartsWithHttp(url) {
+        var regexp = /((http|https|ftp)\:\/\/).+/;
+        return regexp.test(url);
+    }
+
+    function checkForUrlInTaskDescription(task) {
+        var texte = task.description;
+        var words = texte.split(" ");
+        for (var i = 0; i < words.length; i++) {
+            if (isUrl(words[i])) {
+                var link = {};
+                if (urlStartsWithHttp(words[i])) {
+                    link = '<a href="' + words[i] + '" target="_blank" rel="nofollow">' + words[i] + '</a>';
+                }
+                else {
+                    link = '<a href="http://' + words[i] + '" target="_blank" rel="nofollow">' + words[i] + '</a>';
+                }
+                words[i] = words[i].replace(words[i], link);
+            }
+        }
+        task.description = words.join(" ");
+    }
+
+    /*************
+     *** Lists ***
+     *************/
+    $scope.initLists = function () {
+        $scope.data = sharedService.data;
+        $scope.isCollapsed = true;
+        $scope.toFocus = false;
+        ListService.query(
+            function (res) { //success
+                $scope.data.Lists = res;
+                $scope.loaded = true;
+                spinner.stop();
+            },
+            function (err) { //error
+            });
+    };    
+
+    $scope.showDetails = function(listItem)
+    {
+        $scope.data.list = listItem;
+        if($location.path() === '/todoList/details')
+            $route.reload();
+        else
+            $location.path('/todoList/details');
+    };
 
     function createList() {
         $scope.isCollapsed = true;
@@ -81,95 +141,6 @@
             },
             function (err) { //error
             });
-    };
-
-    function createTask() {
-        $scope.isCollapsed = true;
-        $scope.newTask.listId = $scope.data.list._id;
-        var newTask = new TaskService($scope.newTask);
-        newTask.$save(
-            function (task) { //success
-                if (!task)
-                    $log.log('Impossible to create new task entry');
-                else {
-                    $scope.tasksPresent = true;
-                    $scope.data.Tasks.push(task);
-                    $scope.newTask = {};
-                }
-            },
-            function (err) { //error
-            });
-    };
-    
-    function updateTask() {
-        $scope.isCollapsed = true;
-        $scope.newTask.$save({Id: $scope.newTask._id}, 
-            function (task) { //success
-                if (!task)
-                    $log.log('Impossible to update task entry');
-            },
-            function (err) { //error
-            });
-    };
-    
-    $scope.initLists = function () {
-        $scope.data = sharedService.data;
-        $scope.isCollapsed = true;
-        $scope.toFocus = false;
-        ListService.query(
-            function (res) { //success
-                $scope.data.Lists = res;
-                $scope.loaded = true;
-                spinner.stop();
-            },
-            function (err) { //error
-            });
-    };
-
-    $scope.showDetails = function(listItem)
-    {
-        $scope.data.list = listItem;
-        if($location.path() === '/todoList/details')
-            $route.reload();
-        else
-            $location.path('/todoList/details');
-    };
-
-    $scope.initTasks = function () {
-        $scope.toFocus = false;
-        $scope.data = sharedService.data;
-        TaskService.query({ listId: $scope.data.list._id },
-            function (tasks) { //success
-                if (tasks.length > 0) { $scope.tasksPresent = true; }
-                for (idx in tasks) {
-                    if (tasks[idx].done) { $scope.doneTasksPresent = true; }
-                }
-                $scope.data.Tasks = tasks;
-            },
-            function (err) { //error
-            }
-        );
-    }
-
-    $scope.action = function () {
-        if ($scope.buttonText == 'Create')
-            createList();
-        else
-            updateList();
-        $scope.toFocus = false;
-    };
-
-    $scope.actionTask = function () {
-        if ($scope.buttonText == 'Create')
-            createTask();
-        else
-            updateTask();
-        $scope.toFocus = false;
-    };
-    
-    $scope.cancel = function () {
-        $scope.isCollapsed = true;
-        $scope.toFocus = false;
     };
 
     $scope.removeList = function (list,e) {
@@ -200,6 +171,71 @@
         $scope.isCollapsed = false;
     };
 
+    $scope.showNewListPanel = function () {
+        $scope.newlist = {};
+        $scope.buttonText = 'Create';
+        $scope.isCollapsed = false;
+        $scope.toFocus = true;
+    };
+
+    /*************
+     *** Tasks ***
+     *************/
+    $scope.initTasks = function () {
+        $scope.toFocus = false;
+        $scope.data = sharedService.data;
+        TaskService.query({ listId: $scope.data.list._id },
+            function (tasks) { //success
+                if (tasks.length > 0) { $scope.tasksPresent = true; }
+                for (idx in tasks) {
+                    if (tasks[idx].done) { $scope.doneTasksPresent = true; }
+                }
+                $scope.data.Tasks = tasks;
+            },
+            function (err) { //error
+            }
+        );
+    }
+
+    $scope.actionTask = function () {
+        if ($scope.buttonText == 'Create')
+            createTask();
+        else
+            updateTask();
+        $scope.toFocus = false;
+    };
+    
+    function createTask() {
+        $scope.isCollapsed = true;
+        $scope.newTask.listId = $scope.data.list._id;
+        checkForUrlInTaskDescription($scope.newTask);
+        var newTask = new TaskService($scope.newTask);
+        newTask.$save(
+            function (task) { //success
+                if (!task)
+                    $log.log('Impossible to create new task entry');
+                else {
+                    $scope.tasksPresent = true;
+                    $scope.data.Tasks.push(task);
+                    $scope.newTask = {};
+                }
+            },
+            function (err) { //error
+            });
+    };
+    
+    function updateTask() {
+        $scope.isCollapsed = true;
+        checkForUrlInTaskDescription($scope.newTask);
+        $scope.newTask.$save({Id: $scope.newTask._id}, 
+            function (task) { //success
+                if (!task)
+                    $log.log('Impossible to update task entry');
+            },
+            function (err) { //error
+            });
+    };
+
     $scope.removeTask = function (task) {
         var id = task._id;
         TaskService.delete({ Id: id }, 
@@ -224,19 +260,29 @@
         $scope.isCollapsed = false;
     };
 
-    $scope.showNewListPanel = function () {
-        $scope.newlist = {};
-        $scope.buttonText = 'Create';
-        $scope.isCollapsed = false;
-        $scope.toFocus = true;
-    };
-
     $scope.showNewTaskPanel = function () {
         $scope.newTask = {};
         $scope.newTask.importance = 1;
         $scope.buttonText = 'Create';
         $scope.isCollapsed = false;
         $scope.toFocus = true;
+    };
+       
+    /**************
+     *** Common ***
+     **************/
+
+    $scope.action = function () {
+        if ($scope.buttonText == 'Create')
+            createList();
+        else
+            updateList();
+        $scope.toFocus = false;
+    };
+    
+    $scope.cancel = function () {
+        $scope.isCollapsed = true;
+        $scope.toFocus = false;
     };
 
     $scope.chgState = function (item) {
